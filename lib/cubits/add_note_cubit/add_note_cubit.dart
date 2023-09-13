b1e23fb8798c645b225786c91bcca96fe41aecfc
chaymaegh <ghoudatchaymae@gmail.com>
2023-09-13 19:19:39 +0100

    add addNote method to add note cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,32 +1,35 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:mission_list/constants.dart';
 import 'package:mission_list/cubits/add_note_cubit/add_note_state.dart';
 import 'package:mission_list/models/note_model.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
+  AddNoteCubit() : super(AddNoteState(isLoading: true));
 
+  Future<void> loadInitialData() async {
+    final stableState = state;
+    try {
+      emit(state.copyWith(isLoading: true));
 
-	AddNoteCubit() : super(AddNoteState(isLoading: true));
-	
-	Future<void> loadInitialData() async {
-		final stableState = state;
-		try {
-		  emit(state.copyWith(isLoading: true));
-	
-		  // TODO your code here
-	
-		  emit(state.copyWith(isLoading: false));
-		} catch (error) {
-		  emit(state.copyWith(error: error.toString()));
-		  emit(stableState.copyWith(isLoading: false));
-		}
-	}
-addNote(NoteModel note){
-  
+      // TODO your code here
+
+      emit(state.copyWith(isLoading: false));
+    } catch (error) {
+      emit(state.copyWith(error: error.toString()));
+      emit(stableState.copyWith(isLoading: false));
+    }
+  }
+
+  addNote(NoteModel note) async {
+    emit(AddNoteLoading());
+
+    try {
+      var notesBox = Hive.box<NoteModel>(KNotesBox);
+      emit(AddNoteSuccess());
+      await notesBox.add(note);
+    } catch (e) {
+      AddNoteNoteFailure(e.toString());
+    }
+  }
 }
-
-
-  
-}
-
-
-
