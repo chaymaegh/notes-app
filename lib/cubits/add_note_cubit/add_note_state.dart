ea537de195dd7ccd5df925260eb60edc22aa1c1e
chaymaegh <ghoudatchaymae@gmail.com>
2023-09-13 15:08:15 +0100

    add add notes state

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -1,19 +1,30 @@
 class AddNoteState {
-	final bool isLoading;
-	final String? error;
-	  
-	const AddNoteState({
-		this.isLoading = false,
-		this.error,
-	});
-	  
-	AddNoteState copyWith({
-		bool? isLoading,
-		String? error,
-	}) {
-		return AddNoteState(
-			isLoading: isLoading ?? this.isLoading,
-			error: error ?? this.error,
-		);
-	}
+  final bool isLoading;
+  final String? error;
+
+  const AddNoteState({
+    this.isLoading = false,
+    this.error,
+  });
+
+  AddNoteState copyWith({
+    bool? isLoading,
+    String? error,
+  }) {
+    return AddNoteState(
+      isLoading: isLoading ?? this.isLoading,
+      error: error ?? this.error,
+    );
+  }
+}
+
+class AddNoteInitial extends AddNoteState {}
+
+class AddNoteLoading extends AddNoteState {}
+
+class AddNoteSuccess extends AddNoteState {}
+
+class AddNoteNoteFailure extends AddNoteState {
+  final String errMessage;
+  AddNoteNoteFailure(this.errMessage);
 }
